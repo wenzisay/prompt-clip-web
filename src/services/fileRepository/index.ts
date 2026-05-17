2755211252d48eb74bd87e5391dffb690960cd4d
wenzisay <hashcode@hotmail.com>
2026-05-18 05:01:17 +0800

    feat: add tauri file repository adapter

--- src/services/fileRepository/index.ts
+++ src/services/fileRepository/index.ts
@@ -1,4 +1,12 @@
+import { tauriFileRepository } from './tauriFileRepository';
+import { webFileRepository } from './webFileRepository';
+
 export type { FileRepository } from './types';
 export { createFakeFileRepository, createFakeWorkspace } from './fakeFileRepository';
+export { tauriFileRepository } from './tauriFileRepository';
 export { webFileRepository } from './webFileRepository';
-export { webFileRepository as fileRepository } from './webFileRepository';
+
+export const fileRepository =
+  typeof window !== 'undefined' && '__TAURI_INTERNALS__' in window
+    ? tauriFileRepository
+    : webFileRepository;
